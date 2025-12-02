f54de9d0702ffb8242196ba04e07cb6156a87b5f
Rubaid07 <rubaidindu@gmail.com>
2025-12-02 21:53:42 +0600

    create CRUD operation for TODO task

--- src/server.ts
+++ src/server.ts
@@ -179,6 +179,7 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
 });
 
 //  ------------------TODOS CRUD------------------
+// post todo
 app.post("/todos", async (req: Request, res: Response) => {
   const { user_id, title } = req.body;
   try {
@@ -217,6 +218,65 @@ app.get("/todos", async (req: Request, res: Response) => {
   }
 });
 
+// Get single todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM todos WHERE id = $1", [
+      req.params.id,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Failed to fetch todo" });
+  }
+});
+
+// Update todo
+app.put("/todos/:id", async (req, res) => {
+  const { title, completed } = req.body;
+
+  try {
+    const result = await pool.query(
+      "UPDATE todos SET title=$1, completed=$2 WHERE id=$3 RETURNING *",
+      [title, completed, req.params.id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Failed to update todo" });
+  }
+});
+
+// Delete todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "DELETE FROM todos WHERE id=$1 RETURNING *",
+      [req.params.id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+
+    res.json({ success: true, message: "Todo deleted", data: null });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Failed to delete todo" });
+  }
+});
+
+// not found route
 app.use((req: Request, res: Response) => {
   res.status(404).json({
     success: false,
